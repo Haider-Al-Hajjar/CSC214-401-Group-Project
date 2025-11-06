c6e1f54f9683cb79712466cf5d1e07d2640b4c5a
Haider M Al Hajjar <halhajja@dtcc.edu>
2025-11-06 14:57:49 -0500

    Started the display dialogue method. Just had the idea to split the dialogue into chunks for ease of display.

--- FantasySortingGame/src/main/java/com/example/fantasysortinggame/storyhandlers/DialogueBoxController.java
+++ FantasySortingGame/src/main/java/com/example/fantasysortinggame/storyhandlers/DialogueBoxController.java
@@ -5,30 +5,51 @@ import com.example.fantasysortinggame.datatypes.Dialogue;
 
 public class DialogueBoxController {
     private final Database database;
-    private Dialogue dialogue = null;
-
+    private Dialogue currentDialogue = null;
+    private String newNpcInDialogueCharSequence = "newNPC:";
     public DialogueBoxController(Database database) {
         this.database = database;
     }
 
     public void runDialogue(Dialogue dialogue) {
-        /*
-                this.dialogue = dialogue;
-                displayDialogue(dialogue, "")
-                dialogue.setHasHappened(true)
-        */
+        currentDialogue = dialogue;
+        displayDialogue(currentDialogue, "");
     }
+
     public void displayDialogue(Dialogue dialogue, String theStorySoFar) {
+        /*
+            NOTE TO SELF: CUT THE DIALOGUE INTO CHUNKS ELSEWHERE AND THEN JUST DISPLAY A CHUNK!!!!!
+         */
+
+
+        if (theStorySoFar.equals(dialogue.getDialogue())) {
+            currentDialogue.setHappened(true);
+            closeDialogue();
+        }
+        else {
+            int startIndex = dialogue.getDialogue().indexOf(theStorySoFar) + theStorySoFar.length();
+            String displayString = dialogue.getDialogue().substring(startIndex, (startIndex+20));
+            // later, the displaystring will display an appropriate number of characters.
+            if (displayString.contains(newNpcInDialogueCharSequence)) {
+
+            }
+        }
         /*
             if thestory so far != dialogue.getDialogue
                 update the screen with dialogue after theStorySoFar
                     if the string is longer than the window, then stop at a specific point.
             else
+                currentDialogue.setHappened(true);
                 closeDialogue
          */
+
     }
 
-    public void onNextButtonClickHandler(){
+    public void closeDialogue() {
+        // This will eventually just delete the stage showing the dialogue.
+    }
+
+    public void onNextButtonClickHandler() {
         /*
             get the text from the display and store in
             String theStorySoFar
