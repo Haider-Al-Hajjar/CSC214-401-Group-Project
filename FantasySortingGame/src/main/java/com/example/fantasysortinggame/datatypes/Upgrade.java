835b97a64e510ad72d44f802ba0daef6fbbfb673
cslemmer <cslemmer@dtcc.edu>
2025-11-20 15:32:58 -0500

    Finished up BuyPhaseController

--- FantasySortingGame/src/main/java/com/example/fantasysortinggame/datatypes/Upgrade.java
+++ FantasySortingGame/src/main/java/com/example/fantasysortinggame/datatypes/Upgrade.java
@@ -2,12 +2,36 @@ package com.example.fantasysortinggame.datatypes;
 
 public class Upgrade {
     String ability;
-    double cost;
+    int cost;
     boolean isBought;
 
-    public Upgrade(String ability, double cost, boolean isBought) {
+    public Upgrade(String ability, int cost, boolean isBought) {
         this.ability = ability;
         this.cost = cost;
         this.isBought = isBought;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getAbility() {
+        return ability;
+    }
+
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+
+    public boolean isBought() {
+        return isBought;
+    }
+
+    public void setBought(boolean bought) {
+        isBought = bought;
     }
 }
